30494e45b609dbed1baf7007037778e2aa20acb9
Rahul Dev <rahul.e.dev@gmail.com>
2026-01-24 18:57:08 -0500

    use ray for preprocessing

--- utils.py
+++ utils.py
@@ -2,10 +2,11 @@ import random
 
 import numpy as np
 import pandas as pd
+import ray
 import rdkit.Chem as Chem
+from rdkit.Chem import Descriptors
 import torch
-from rdkit.Chem.Descriptors import CalcMolDescriptors
-from rdkit.Chem.MolStandardize import rdMolStandardize # type: ignore
+from rdkit.Chem.MolStandardize import rdMolStandardize  # type: ignore
 from rdkit.Chem.Scaffolds.MurckoScaffold import MurckoScaffoldSmiles
 from rdkit.rdBase import BlockLogs
 
@@ -21,7 +22,7 @@ def set_seeds(seed):
 
 def standardize(smiles):
     with BlockLogs():
-        params = Chem.SmilesParserParams() # type: ignore
+        params = Chem.SmilesParserParams()  # type: ignore
         params.removeHs = False
         mol = Chem.MolFromSmiles(smiles, params)  # type: ignore
         if mol is None:
@@ -34,29 +35,63 @@ def standardize(smiles):
         return uncharged_parent_clean_mol
 
 
-def generate_features(df):
-    with BlockLogs():
-        feats = pd.DataFrame.from_records(df["mol"].map(CalcMolDescriptors).tolist())
-        feats.columns = [f"feat_{f}" for f in feats.columns]
-        df = pd.concat(
-            [
-                df.reset_index(drop=True),
-                feats,
-            ],
-            axis=1,
-        )
-
-    return df
-
-
 def mol_to_inchi(mol):
     with BlockLogs():
         return Chem.MolToInchi(mol)
 
 
 def get_scaffold(mol) -> str:
-    smi = Chem.MolToSmiles(mol) # type: ignore
-    scaffold: str = MurckoScaffoldSmiles(smi) # type: ignore
+    smi = Chem.MolToSmiles(mol)  # type: ignore
+    scaffold: str = MurckoScaffoldSmiles(smi)  # type: ignore
     if len(scaffold) == 0:
         scaffold = smi
     return scaffold
+
+
+def getMolDescriptors(mol):
+    """calculate the full list of descriptors for a molecule missingVal is used if the descriptor cannot be calculated"""
+    res = {}
+    for nm, fn in Descriptors._descList:
+        # some of the descriptor fucntions can throw errors if they fail, catch those here:
+        with BlockLogs():
+            try:
+                val = fn(mol)
+            except Exception as e:
+                val = None
+
+        res[nm] = val
+    return res
+
+
+def preprocess_row(row):
+    smi = row["smiles"]
+    mol = standardize(smi)
+
+    if mol is None:
+        return dict(inchi=None, scaffold=None) | {
+            name: None for name in Descriptors.descList
+        }
+
+    return dict(
+        inchi=mol_to_inchi(mol), scaffold=get_scaffold(mol)
+    ) | getMolDescriptors(mol)
+
+
+def preprocess_ray(df):
+    assert "smiles" in set(df.columns)
+
+    df = (
+        ray.data.from_pandas(df, override_num_blocks=len(df) // 64)
+        .map(lambda row: row | preprocess_row(row))
+        # filter any rows that have None. This includes any mols that have NaN descriptor values
+        .filter(lambda row: not any(pd.isna(v) for v in row.values()))
+        .to_pandas()
+    )
+
+    df = df.groupby(["inchi"]).filter(lambda x: len(x) == 1).reset_index(drop=True)
+    clusters, _ = pd.factorize(df["scaffold"])
+    df["cluster"] = pd.Series(clusters)
+    df["mol"] = df["inchi"].map(Chem.MolFromInchi)
+    df = df.drop(["smiles", "inchi", "scaffold"], axis=1)
+    return df
+
