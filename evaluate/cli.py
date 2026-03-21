8e7a58d2f54cef53bcdd595c4e39971f583ab540
Rahul Dev <rahul.e.dev@gmail.com>
2026-03-21 09:21:02 -0400

    bugfix: pass use feats flag properly

--- evaluate/cli.py
+++ evaluate/cli.py
@@ -7,7 +7,7 @@ from data import SupportedDatasets
 from models.config import BaselineConfig, DeltapropConfig
 
 
-def prepare_dataset(dataset: SupportedDatasets):
+def prepare_dataset(dataset: SupportedDatasets, generate_features: bool):
     # Lazy import here to prevent cli startup from being slow
     import ray
 
@@ -17,7 +17,7 @@ def prepare_dataset(dataset: SupportedDatasets):
     ray.init(ignore_reinit_error=True, num_cpus=4)
 
     df, df_classification_threshold = load_dataset(dataset)
-    df = preprocess_ray(df)
+    df = preprocess_ray(df, generate_features=generate_features)
 
     ray.shutdown()
 
@@ -49,7 +49,9 @@ def baseline(
         run = wandb.init(project=wandb_cf.project_name, tags=list(tags))
         run.mark_preempting()
 
-    df, df_classification_threshold = prepare_dataset(dataset)
+    df, df_classification_threshold = prepare_dataset(
+        dataset, generate_features=train_cf.use_feats
+    )
     result_iter = train_and_evaluate(
         df=df,
         df_classification_threshold=df_classification_threshold,
@@ -99,7 +101,9 @@ def deltaprop(
         run = wandb.init(project=wandb_cf.project_name, tags=list(tags))
         run.mark_preempting()
 
-    df, df_classification_threshold = prepare_dataset(dataset)
+    df, df_classification_threshold = prepare_dataset(
+        dataset, generate_features=train_cf.use_feats
+    )
     result_iter = train_and_evaluate(
         df=df,
         df_classification_threshold=df_classification_threshold,
@@ -111,7 +115,7 @@ def deltaprop(
     for result_dict in result_iter:
         if isinstance(wandb_cf, WandbEnabled):
             model_name_suffix = wandb_cf.model_name_suffix
-            model_name = "deltaprop" + (model_name_suffix if model_name_suffix else "")
+            model_name = "deltaprop" + (f"-{model_name_suffix}" if model_name_suffix else "")
             wandb.log(  # type: ignore
                 result_dict
                 | asdict(model_cf)
