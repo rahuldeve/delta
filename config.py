cb6b751425798e8c86954f5d9642dc2d67196030
Rahul Dev <rahul.e.dev@gmail.com>
2026-06-12 10:57:28 -0400

    remove old wandb project default

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class WandbDisabled:
 
 @dataclass
 class WandbEnabled:
-    project_name: str = "evaluate_tba"
+    project_name: str
     tags: list[str] = field(default_factory=list)
     model_name_suffix: str|None = None
 
