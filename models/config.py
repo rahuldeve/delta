7fb586708536ad87eef1dc68ef062e7848ed02bd
Rahul Dev <rahul.e.dev@gmail.com>
2026-08-21 04:10:35 -0400

    update frac_hard

--- models/config.py
+++ models/config.py
@@ -31,7 +31,7 @@ class DeltapropConfig(ModelConfig):
     batch_norm: bool = False
     interaction_dropout: float = 0.1
     candidate_size: int = 32
-    frac_hard: float = 0.2
+    frac_hard: float = 0.5
     hard_band: float = 1.0
     use_chameleon_mp: bool = False
 
